8b61023853fbfa2b838b57d5a754bba583174aba
Kateryna_Platonova <katyaplato@gmail.com>
2023-08-02 13:48:40 +0200

    Create a Spring project

---
+++ backend/src/main/java/com/example/travellogapp/TravelLogAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.travellogapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelLogAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelLogAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/travellogapp/TravelLogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travellogapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelLogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
