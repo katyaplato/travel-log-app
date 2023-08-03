62edfe12d149d44a69ad14ee78bbe2962b85f61b
Kateryna_Platonova <katyaplato@gmail.com>
2023-08-03 13:04:52 +0200

    Create a subscription entity

--- backend/src/main/java/com/example/backend/models/Subscription.java
+++ backend/src/main/java/com/example/backend/models/Subscription.java
@@ -1,13 +1,28 @@
 package com.example.backend.models;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDate;
 
 @Entity
 @Setter
 @Getter
 @RequiredArgsConstructor
+@Table(name="SUBSCRIPTIONS")
 public class Subscription {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "subscriber_id")
+    private User subscriber;
+
+    @ManyToOne
+    @JoinColumn(name = "user_to_subscribe_id")
+    private User userToSubscribe;
+    private LocalDate subscriptionDate;
+
 }
