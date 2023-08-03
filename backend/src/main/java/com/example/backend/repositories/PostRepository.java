5870a5ea736f90495e05af06a7818ead36fae9cf
Kateryna_Platonova <katyaplato@gmail.com>
2023-08-03 16:33:08 +0200

    Update PostRepository with a new method

--- backend/src/main/java/com/example/backend/repositories/PostRepository.java
+++ backend/src/main/java/com/example/backend/repositories/PostRepository.java
@@ -3,5 +3,8 @@ package com.example.backend.repositories;
 import com.example.backend.models.Post;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface PostRepository extends CrudRepository <Post, Long> {
+    List<Post> findByLocation(String location);
 }
