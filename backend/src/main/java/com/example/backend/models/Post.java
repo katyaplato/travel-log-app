9efdee322aae00989d14d7dc8bb135e02aa753fd
Kateryna_Platonova <katyaplato@gmail.com>
2023-08-03 12:54:57 +0200

    Create a Post entity

---
+++ backend/src/main/java/com/example/backend/models/Post.java
@@ -0,0 +1,38 @@
+package com.example.backend.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Data
+@AllArgsConstructor
+@Table(name="POSTS")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private String location;
+    private LocalDate creationDate;
+    @ElementCollection
+    private List<String> images;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
+    public Post() {
+
+    }
+}
--- backend/src/main/java/com/example/backend/models/User.java
+++ backend/src/main/java/com/example/backend/models/User.java
@@ -9,6 +9,7 @@ import lombok.Setter;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
