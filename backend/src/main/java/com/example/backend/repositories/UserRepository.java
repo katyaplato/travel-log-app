9d76143c9cda179eb7d69771fa329a04853b5f77
Kateryna_Platonova <katyaplato@gmail.com>
2023-08-06 14:26:42 +0200

    Update UserRepository

--- backend/src/main/java/com/example/backend/repositories/UserRepository.java
+++ backend/src/main/java/com/example/backend/repositories/UserRepository.java
@@ -2,6 +2,9 @@ package com.example.backend.repositories;
 
 import com.example.backend.models.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository <User, Long> {
+    User findUserByUsername(String username);
 }
