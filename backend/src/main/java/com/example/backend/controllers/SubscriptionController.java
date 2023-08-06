7cb7a2a036c22c395ecf0d5a0b992913864a335a
Kateryna_Platonova <katyaplato@gmail.com>
2023-08-06 14:19:29 +0200

    Update SubscriptionController with endpoint subscribe to user

--- backend/src/main/java/com/example/backend/controllers/SubscriptionController.java
+++ backend/src/main/java/com/example/backend/controllers/SubscriptionController.java
@@ -1,10 +1,13 @@
 package com.example.backend.controllers;
 
+import com.example.backend.models.Subscription;
 import com.example.backend.repositories.SubscriptionRepository;
 import com.example.backend.services.SubscriptionService;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/subscription")
@@ -17,4 +20,17 @@ public class SubscriptionController {
         this.subscriptionRepository = subscriptionRepository;
         this.subscriptionService = subscriptionService;
     }
+    @PostMapping("/subscribe/{userIdToSubscribe}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Subscription> subscribeToUser(@PathVariable Long userIdToSubscribe) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String loggedInUsername = authentication.getName();
+
+        Subscription subscription = subscriptionService.subscribeUser(loggedInUsername, userIdToSubscribe);
+
+        if (subscription != null) {
+            return ResponseEntity.status(HttpStatus.CREATED).body(subscription);
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
 }
