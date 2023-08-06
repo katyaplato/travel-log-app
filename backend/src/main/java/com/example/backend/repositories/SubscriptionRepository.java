da037eb08746ede5d82ff942b000a057942e43bb
Kateryna_Platonova <katyaplato@gmail.com>
2023-08-06 14:24:18 +0200

    Update SubcriptionRepository

--- backend/src/main/java/com/example/backend/repositories/SubscriptionRepository.java
+++ backend/src/main/java/com/example/backend/repositories/SubscriptionRepository.java
@@ -1,7 +1,11 @@
 package com.example.backend.repositories;
 
 import com.example.backend.models.Subscription;
+import com.example.backend.models.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SubscriptionRepository extends CrudRepository <Subscription,Long> {
+    Subscription findBySubscriberAndUserToSubscribe(User subscriber, User userToSubscribe);
 }
