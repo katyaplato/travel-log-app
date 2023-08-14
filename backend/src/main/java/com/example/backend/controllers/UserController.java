e085887b9d3ca82c888f035af1e6097c12d06a8a
Kateryna_Platonova <katyaplato@gmail.com>
2023-08-14 22:50:38 +0200

    Update with ApiResponses

--- backend/src/main/java/com/example/backend/controllers/UserController.java
+++ backend/src/main/java/com/example/backend/controllers/UserController.java
@@ -4,6 +4,8 @@ import com.example.backend.dtos.UserGetInfoDTO;
 import com.example.backend.models.Post;
 import com.example.backend.models.Subscription;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,13 +20,25 @@ public interface UserController {
 
     @GetMapping("/{id}/posts")
     @Operation(summary = "Get all posts by user ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved posts by user ID"),
+            @ApiResponse(responseCode = "404", description = "User not found")
+    })
     ResponseEntity<List<Post>> getAllPostsByUser(@PathVariable Long id);
 
     @GetMapping("/{id}/subscriptions")
     @Operation(summary = "Get all subscriptions by user ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved subscriptions by user ID"),
+            @ApiResponse(responseCode = "404", description = "User not found")
+    })
     ResponseEntity<List<Subscription>> getAllSubscriptions(@PathVariable Long id);
 
     @GetMapping("/{id}")
     @Operation(summary = "Get user info by user ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved user info by user ID"),
+            @ApiResponse(responseCode = "404", description = "User not found")
+    })
     UserGetInfoDTO getUserInfo(@PathVariable Long id);
 }
