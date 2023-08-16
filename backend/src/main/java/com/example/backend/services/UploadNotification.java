202c39ad7566ed480cf853ef349e4a6b30217f4b
Kateryna_Platonova <katyaplato@gmail.com>
2023-08-16 17:19:08 +0200

    fix upload notifications

--- backend/src/main/java/com/example/backend/services/UploadNotification.java
+++ backend/src/main/java/com/example/backend/services/UploadNotification.java
@@ -14,22 +14,23 @@ public class UploadNotification {
     private final EmailService emailService;
     private final UploadService uploadService; // You need to define your UploadService
     private final SubscriptionService subscriptionService;
+    private final UserService userService;
 
 
-    @Scheduled(fixedRate = 60000) // Check for new uploads every minute
+    @Scheduled(fixedRate = 60000)
     public void sendUploadNotifications() {
-        // Retrieve new uploads using your UploadService
+
         List<Upload> newUploads = uploadService.getNewUploads();
 
         for (Upload upload : newUploads) {
-            String subscribedUserName = upload.getUser().getUsername(); // Assuming you have a User associated with the upload
-            List<String> subscriberEmails = subscriptionService.getSubscriberEmailsForUser(subscribedUserName);
+            String subscribedUserName = upload.getUser().getUsername();
+            List<String> subscriberEmails = userService.getSubscriberEmailsForUser(subscribedUserName);
 
             for (String subscriberEmail : subscriberEmails) {
-                // Construct upload details (e.g., title, description)
+
                 String uploadDetails = "New Upload: " + upload.getTitle() + "\nDescription: " + upload.getDescription();
 
-                // Send upload notification to each subscriber's email
+
                 emailService.sendUploadNotificationToSubscribers(subscriberEmail, subscribedUserName, uploadDetails);
             }
         }
